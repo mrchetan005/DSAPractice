42dd5938aa031d104ddcd0a740e5fecb1cb66d9d
Chetan <mr.chetan005@gmail.com>
2023-01-31 18:56:14 +0530

    max-consecutive-ones-iii

---
+++ Arrays/MaxConsecutiveOnesIII.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+/*
+       * LEETCODE: 1004 Max Consecutive Ones III
+       * https://leetcode.com/problems/max-consecutive-ones-iii
+*/
+
+/*
+       * we can do this with sliding window algorithm
+       * take two pointers left and right
+       * if (arr[left] == 0) then decrement k (i.e. flip's count) 
+       * if m becomes negative keep moving left until arr[left] equals 0
+       * if we found arr[left] equals 0 increment k
+       * return (right - left) because this window contains maximum number of ones produced by flipping at most k 0s...
+*/
+
+public class MaxConsecutiveOnesIII {
+    public static int longestOnes(int[] nums, int k) {
+        int left = 0, right = 0;
+        while (right < nums.length) {
+            if (nums[right] == 0) {
+                k--;
+            }
+            if (k < 0 && nums[left++] == 0) {
+                k++;
+            }
+            right++;
+        }
+        return right - left;
+    }
+
+    // driver code
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 };
+        int k = 2;
+        System.out.println(longestOnes(arr, k));
+    }
+}
